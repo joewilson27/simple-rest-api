5fc0a655a91fca241de5d598294e2ca716308f8d
Joe Wilson <wilsonmangunsong@gmail.com>
2020-04-01 10:27:17 +0700

    web api with sql server

---
+++ StudentServices/StudentServices/Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StudentDataAccess;
+
+namespace StudentServices.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        public IEnumerable<Student> Get()
+        {
+            using (StudentDetailsEntities entities = new StudentDetailsEntities())
+            {
+                return entities.Students.ToList();
+            }
+        }
+
+        public Student Get(int id)
+        {
+            using (StudentDetailsEntities entities = new StudentDetailsEntities())
+            {
+                return entities.Students.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,13 @@
+
+
+Before create this project, we have to create our database in SQL Server using SQL Server Management Studio.
+I have created the database name  'StudentDetails' and table name 'Student' that described below:
+ID INT NOT NULL
+FirstName Char(50)
+LastName Char(50)
+Gender Char(10)
+Age INT 
+After created the table, you can insert some dummies data.
+
+Using entitiyframework, we use DBFirst in this project.
+
